409e079fd67e95904cfb5305dae41a5d3c1c663a
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2026-06-09 13:56:38 +0530

    feat: added get post with slug api

--- routes/posts.js
+++ routes/posts.js
@@ -8,8 +8,18 @@ const db = pgp(process.env.POSTGRES_ADDRESS);
 router.get("/", async (req, res, next) => {
   db.query("SELECT * FROM posts")
     .then((posts) => {
-      console.log(posts);
       return res.json(posts);
+    })
+    .catch((error) => {
+      console.log("ERROR:", error);
+      return res.send("ERROR:", error)
+    });
+});
+
+router.get("/:slug", async (req, res, next) => {
+  db.query("SELECT * FROM posts WHERE slug = $1", req.params.slug)
+    .then((post) => {
+      return res.json(post[0]);
     })
     .catch((error) => {
       console.log("ERROR:", error);
