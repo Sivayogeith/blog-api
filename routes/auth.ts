29a801ef4bb2b800466c1541a9ca5e385532ed43
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2026-07-15 17:40:50 +0530

    fix: register now bcrypts passwords and gives an response

--- routes/auth.ts
+++ routes/auth.ts
@@ -11,24 +11,41 @@ export const authRouter = express.Router();
 const { as } = pgp;
 
 authRouter.post("/register", async (req, res, next) => {
-  const { username, password } = req.body
-  
+  const { username, password } = req.body;
+
   if (username.length < 4 || password.length < 8) {
-    return res.status(400).send("Username must be at least 4 characters long, and password must be at least 8 characters long")
-  }
-  
-  const doesUserExist = await db.query<User[]>("SELECT * FROM users WHERE username = $1", username)
-  if (doesUserExist.length){
-    return res.status(409).send("Username is already in use :(")
+    return res
+      .status(400)
+      .send(
+        "Username must be at least 4 characters long, and password must be at least 8 characters long",
+      );
   }
 
-  db.one<User>("INSERT INTO users (${this:name}) VALUES (${this:csv})", {username, password}).then(({username, id, isAdmin}) => {
-    req.session.username = username
-    req.session.userId = id
-    req.session.isAdmin = isAdmin
-    return res.status(200).send("Successfully registered user :D")
-  }).catch(next)
-})
+  const doesUserExist = await db.query<User[]>(
+    "SELECT * FROM users WHERE username = $1",
+    username,
+  );
+  if (doesUserExist.length) {
+    return res.status(409).send("Username is already in use :(");
+  }
+
+  bcrypt.hash(password, 10, (err, hashedPass) =>
+    err
+      ? next(err)
+      : db
+          .one<{ id: number }>(
+            "INSERT INTO users (${this:name}) VALUES (${this:csv}) RETURNING id",
+            { username, password: hashedPass },
+          )
+          .then(({ id }) => {
+            req.session.username = username;
+            req.session.userId = id;
+            req.session.isAdmin = false;
+            return res.status(200).send("Successfully registered user :D");
+          })
+          .catch(next),
+  );
+});
 
 authRouter.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
@@ -47,7 +64,7 @@ authRouter.post("/login", async (req, res, next) => {
         if (result) {
           req.session.username = username;
           req.session.userId = user.id;
-          req.session.isAdmin = user.isAdmin
+          req.session.isAdmin = user.isAdmin;
           return res.status(200).send("Successfully logged in :D");
         } else {
           return res.status(403).send("Wrong password :(");
@@ -68,7 +85,7 @@ authRouter.get("/me", async (req, res, next) => {
       return res.status(200).json({
         username: session?.sess.username,
         userId: session?.sess.userId,
-        isAdmin: session?.sess.isAdmin
+        isAdmin: session?.sess.isAdmin,
       });
     })
     .catch(next);
@@ -85,12 +102,14 @@ authRouter.get("/logout", authenticated, async (req, res, next) => {
 
 authRouter.post("/edit", adminOnly, async (req, res, next) => {
   const cs = new (pgp().helpers.ColumnSet)(["username"], {
-    table: "users"
+    table: "users",
   });
   const where = as.format("WHERE id = $1", req.session.userId);
-  const update = `${pgp().helpers.update(req.body, cs)} ${where}`
-  db.none(update).then(() => {
-    req.session.username = req.body.username
-    return res.status(200).send("Successfully edited your information :D")
-  }).catch(next);
+  const update = `${pgp().helpers.update(req.body, cs)} ${where}`;
+  db.none(update)
+    .then(() => {
+      req.session.username = req.body.username;
+      return res.status(200).send("Successfully edited your information :D");
+    })
+    .catch(next);
 });
