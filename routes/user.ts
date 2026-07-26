2b5a266f18fc68c8714b8e0ff519cc4ca61df865
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2026-07-26 12:45:46 +0530

    feat: added /user/:username/stats API returning total post and comments made by the user

--- routes/user.ts
+++ routes/user.ts
@@ -16,3 +16,17 @@ userRouter.get("/:username", (req, res, next) => {
     })
     .catch(next);
 });
+
+userRouter.get("/:username/stats", (req, res, next) => {
+  Promise.all([
+    db.one("SELECT count(*) FROM posts WHERE author = $1", req.params.username),
+    db.one(`SELECT count(*) FROM comments WHERE "from" = $1`, req.params.username),
+  ])
+    .then(([posts, comments]) => {
+      res.status(200).json({
+        posts: +posts.count,
+        comments: +comments.count,
+      });
+    })
+    .catch(next);
+});
