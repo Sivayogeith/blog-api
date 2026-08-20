1fda8902b346761e88601c86cf40b7e3f58d504d
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2026-08-20 11:58:50 +0530

    feat: added /:id/reply (reply to comment) and /:id/replies (get all replies in a comment)
    also added new col to comments table, "parent" if parent is not null the record is a reply (parent is the id of a comment)

--- routes/comments.ts
+++ routes/comments.ts
@@ -8,17 +8,20 @@ import { limiter } from "../middleware/limiter.js";
 export const commentsRouter = express.Router();
 const { as } = pgp;
 
-commentsRouter.use(limiter)
+commentsRouter.use(limiter);
 
 commentsRouter.post("/:id/edit", authenticated, async (req, res, next) => {
   if (!req.params.id) {
     return res.status(404).send("Please enter the id of the comment!");
   }
 
-  const comment = await db.one<Comment>("SELECT * FROM comments WHERE id = $1", req.params.id)
+  const comment = await db.one<Comment>(
+    "SELECT * FROM comments WHERE id = $1",
+    req.params.id,
+  );
 
   if (comment.from !== req.session.username) {
-    return res.status(403).send("You aren't allowed to edit this comment!")
+    return res.status(403).send("You aren't allowed to edit this comment!");
   }
 
   const cs = new (pgp().helpers.ColumnSet)(["message"], { table: "comments" });
@@ -33,17 +36,43 @@ commentsRouter.post("/:id/edit", authenticated, async (req, res, next) => {
 });
 
 commentsRouter.delete("/:id/delete", authenticated, async (req, res, next) => {
-    if (!req.params.id) {
-        return res.status(404).send("Please enter the id of the comment!")
-    }
-    
-    const comment = await db.one<Comment>("SELECT * FROM comments WHERE id = $1", req.params.id)
+  if (!req.params.id) {
+    return res.status(404).send("Please enter the id of the comment!");
+  }
 
-    if (comment.from !== req.session.username) {
-        return res.status(403).send("You aren't allowed to edit this comment!")
-    }
-    
-    db.none("DELETE FROM comments WHERE id = $1", req.params.id).then(() => {
-        return res.status(200).send("Successfully deleted your comment :D");
-    }).catch(next)
-})
+  const comment = await db.one<Comment>(
+    "SELECT * FROM comments WHERE id = $1",
+    req.params.id,
+  );
+
+  if (comment.from !== req.session.username) {
+    return res.status(403).send("You aren't allowed to edit this comment!");
+  }
+
+  db.none("DELETE FROM comments WHERE id = $1", req.params.id)
+    .then(() => {
+      return res.status(200).send("Successfully deleted your comment :D");
+    })
+    .catch(next);
+});
+
+commentsRouter.get("/:id/replies", async (req, res, next) => {
+  db.query("SELECT * FROM comments WHERE parent = $1", req.params.id)
+    .then((comments) => {
+      return res.status(200).json(comments);
+    })
+    .catch(next);
+});
+
+commentsRouter.post("/:id/reply", authenticated, async (req, res, next) => {
+  const { message } = req.body;
+  db.query(
+    `INSERT INTO comments ("from", "on", message, parent)
+    SELECT $1, "on", $2, id
+    FROM comments
+    WHERE id = $3`,
+    [req.session.username, message, req.params.id],
+  )
+    .then(() => res.status(200).send("Successfully replied!"))
+    .catch(next);
+});
